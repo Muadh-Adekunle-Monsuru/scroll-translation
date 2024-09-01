1de5a0f2aa8c373427525006e5804209cfe58551
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-09-01 09:32:06 +0100

    fixing websocket

--- src/socket.ts
+++ src/socket.ts
@@ -6,4 +6,7 @@ const URL =
 		? 'https://scroll-backend.vercel.app'
 		: 'http://localhost:3000';
 
-export const socket = io(URL, { withCredentials: true });
+export const socket = io(URL, {
+	withCredentials: true,
+	transports: ['websocket'],
+});
